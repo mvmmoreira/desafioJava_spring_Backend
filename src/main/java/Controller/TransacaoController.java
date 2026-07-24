3f1654e95c69aa29eb45328a2f14f4263ef7d47b
Marcos Moreira <vinicius.moreira.nbo@gmail.com>
2026-07-24 20:45:14 -0300

    criando metoda pra delete

--- src/main/java/Controller/TransacaoController.java
+++ src/main/java/Controller/TransacaoController.java
@@ -4,6 +4,7 @@ import dto.TransacaoRequest;
 import model.Transacao;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +30,12 @@ public class TransacaoController {
         }
         transacaoService.addTransacao(new Transacao(request.getValor(), request.getDataHora()));
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> clearTransacoes(){
+        transacaoService.clearTransacao();
+        return ResponseEntity.ok().build();
     }
 
 }
