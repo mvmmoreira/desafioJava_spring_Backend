7af95f147b54c3828cfd36c6a0a9e2c8d49becc5
Marcos Moreira <vinicius.moreira.nbo@gmail.com>
2026-07-24 19:20:32 -0300

    criando model e dto, com as classes transacao e transacaorequest

---
+++ src/main/java/dto/TransacaoRequest.java
@@ -0,0 +1,23 @@
+package dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.OffsetDateTime;
+
+public class TransacaoRequest {
+    @NotNull
+    @Min(0)
+    private double valor;
+
+    @NotNull
+    private OffsetDateTime dataHora;
+
+    public double getValor() {
+        return valor;
+    }
+
+    public OffsetDateTime getDataHora() {
+        return dataHora;
+    }
+}
---
+++ src/main/java/model/Transacao.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.time.OffsetDateTime;
+
+public class Transacao {
+
+    private double valor;
+    private OffsetDateTime dataHora;
+
+
+    public Transacao(final double valor, final OffsetDateTime dataHora){
+        this.valor = valor;
+        this.dataHora = dataHora;
+    }
+
+
+    public double getValor(){
+        return valor;
+    }
+
+    public OffsetDateTime getDataHora(){
+        return dataHora;
+    }
+}
